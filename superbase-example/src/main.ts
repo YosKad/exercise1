abeec528a14dc43c6c98100ddada8f65285a49e2
YosKad <yoskadtl@gmail.com>
2023-06-25 21:19:46 +0300

    add delete option

--- superbase-example/src/main.ts
+++ superbase-example/src/main.ts
@@ -1,22 +1,34 @@
 import "./style.css";
 import { createClient } from "@supabase/supabase-js";
+
 const supabaseUrl = "https://zstywelqfgshdnkypvwi.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzdHl3ZWxxZmdzaGRua3lwdndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcxODYxMzYsImV4cCI6MjAwMjc2MjEzNn0.6Yt8jkgmCWBKH4Od0yeq3_kRQIRZerf2Lj0LAYV8c7U";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzdHl3ZWxxZmdzaGRua3lwdndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcxODYxMzYsImV4cCI6MjAwMjc2MjEzNn0.6Yt8jkgmCWBKH4Od0yeq3_kRQIRZerf2Lj0LAYV8c7U";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function getData() {
-  const { data, error } = await supabase.from("countries").select();
+  const { data, error } = await supabase
+  .from("countries")
+  .select();
 
-  console.log(error);
+  if (error) {
+    console.log(error);
+    return;
+  }
+
   const listCountry = document.getElementById("listCountry");
-  if (data != null) {
+
+  if (data) {
     for (let index = 0; index < data.length; index++) {
-      console.log(data[index]);
       const li = document.createElement("li");
+      const deleteIcon = document.createElement("i");
+
+      deleteIcon.className = "fa-regular fa-trash-can";
+      deleteIcon.dataset.id = data[index].id;
       li.textContent = data[index].name;
       li.id = data[index].id;
+
       listCountry?.appendChild(li);
+      listCountry?.appendChild(deleteIcon);
     }
   }
 }
@@ -26,8 +38,47 @@ getData();
 const submitBtn = document.getElementById("submit");
 
 submitBtn?.addEventListener("click", async () => {
-  const countryName = document.getElementById('countryName') as (HTMLInputElement | null);
+  const countryName = document.getElementById("countryName") as HTMLInputElement | null;
+
+  if (!countryName) {
+    return;
+  }
+
   const { error } = await supabase
-    .from("countries")
-    .insert({name: countryName?.value });
+  .from("countries")
+  .insert([{ name: countryName.value }]);
+
+  if (error) {
+    console.log(error);
+  }
+});
+
+const listCountry = document.getElementById("listCountry");
+
+listCountry?.addEventListener("click", async (event) => {
+  if (event.target instanceof HTMLElement) {
+    const deleteIcon = event.target.closest("i");
+
+    if (deleteIcon) {
+      const countryId = deleteIcon.dataset.id;
+
+      if (!countryId) {
+        return;
+      }
+
+      const { error } = await supabase.from("countries")
+      .delete()
+      .eq("id", countryId);
+
+      if (error) {
+        console.log(error);
+      } else {
+        // Remove the deleted item from the UI
+        const listItem = deleteIcon.parentNode;
+        listItem?.parentNode?.removeChild(listItem);
+        window.location.reload(); // Refresh the page
+
+      }
+    }
+  }
 });
